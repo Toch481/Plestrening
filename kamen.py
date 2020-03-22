ab9317386b98c750c1579b5c73d4b5e085fae621
unknown <dagmar.schierova@gmail.com>
2020-03-22 11:05:53 +0100

    Add kamen.py

---
+++ kamen.py
@@ -0,0 +1,19 @@
+import random
+
+print("Zahraj si semnou kámen nůžky, papír!")
+
+tah_cloveka = input("Zadej kámen, nůžky nebo papír. ")
+moznosti = {1: "kámen", 2: "nůžky", 3: "papír"}
+tah_pocitace = moznosti[random.randint(1, 3)]
+
+print(tah_pocitace)
+
+if tah_cloveka == tah_pocitace:
+    print("Plichta!")
+elif (tah_cloveka == "nůžky" and tah_pocitace == "kámen") or (tah_cloveka == "papír" and tah_pocitace == "nůžky") or (tah_cloveka == "kámen" and tah_pocitace == "papír"):
+    print("Počítač vyhrál!")
+elif tah_cloveka == "nůžky" and tah_pocitace == "papír" (tah_cloveka == "papír" and tah_pocitace == "kámen") or (tah_cloveka == "kámen" and tah_pocitace == "nůžky"):
+    print("Vyhrála jsi!")
+else:
+    print("Špatně.")
+    tah_cloveka = input("Zadej jedno ze slov: kámen, nůžky nebo papír a dej si pozor na diakritiku. ")
